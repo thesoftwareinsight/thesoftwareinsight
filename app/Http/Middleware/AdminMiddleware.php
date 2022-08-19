0a70a59d0525bb476a7b79fee50bcc2054b9ccd2
Muzammil Jumshaid <thisismjey@gmail.com>
2022-08-19 23:01:10 +0500

    firstcommit

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Http\Middleware;
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use app\Models\User;
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Auth::check()){
+            if(Auth::user()->isAdmin()){
+                return $next($request);
+            } else {
+                return redirect('404');
+            }
+        }
+        return redirect('404');
+    }
+}
